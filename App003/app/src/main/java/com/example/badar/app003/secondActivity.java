9e2deb4111451636256090c82897d72f2d85e8a1
badar <badarshahzad54@gmail.com>
2016-09-05 23:44:16 +0500

    App003 is created

---
+++ App003/app/src/main/java/com/example/badar/app003/secondActivity.java
@@ -0,0 +1,16 @@
+package com.example.badar.app003;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by badar on 9/3/16.
+ */
+public class secondActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
